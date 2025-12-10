5a213afd2e84fbe4c02eb47e9d4e77ad3200de4f
watanabeHi <hide3@mild.ocn.ne.jp>
2025-12-10 09:39:42 +0900

    Add fizzbuzz function and normal test

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+exports.fizzBuzz = function (number) {
+    switch (true) {
+        case number % 15 === 0:
+            return "fizzBuzz";
+        case number % 3 === 0:
+            return "fizz";
+        case number % 5 === 0:
+            return "buzz";
+        default:
+            return number;
+    }
+}
---
+++ tests/run.js
@@ -0,0 +1,5 @@
+const {fizzBuzz} = require("../dist");
+
+for (let i = 1; i < 16; i++) {
+    console.log(fizzBuzz(i));
+}
